ab8d497559bcbdf00b78368f03d610b0c146e0f1
Kisan Thapa <kisanthapa33@gmail.com>
2020-08-11 10:01:53 +0545

    Stateful memento design pattern

--- src/com/kisan/Memento/Editor.java
+++ src/com/kisan/Memento/Editor.java
@@ -1,25 +1,22 @@
 package com.kisan.Memento;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Editor {
     private String content;
-    private List<String> history = new ArrayList<>();
 
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
-        history.add(this.content);
         this.content = content;
     }
 
-    public void undo() {
-        if (history.size() >= 1) {
-            content = history.get(history.size() - 1);
-            history.remove(history.size() - 1);
-        }
+    public EditorState createState() {
+        return new EditorState(content);
     }
+
+    public void restoreState(EditorState state) {
+        content = state.getContent();
+    }
+
 }
