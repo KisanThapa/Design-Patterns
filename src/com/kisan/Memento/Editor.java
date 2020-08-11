dd5459baaf2662652acf29416117ff9bbeb3652c
Kisan Thapa <kisanthapa33@gmail.com>
2020-08-11 12:31:08 +0545

    Stateful design pattern

---
+++ src/com/kisan/Memento/Editor.java
@@ -0,0 +1,22 @@
+package com.kisan.Memento;
+
+public class Editor {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restoreState(EditorState state) {
+        content = state.getContent();
+    }
+
+}
---
+++ src/com/kisan/Memento/MultipleAttribute/Main.java
@@ -0,0 +1,26 @@
+package com.kisan.Memento.MultipleAttribute;
+
+public class Main {
+    public static void main(String[] args) {
+        var history = new DocumentHistory();
+        var document = new Document();
+
+        // First document
+        document.setContent("Content 1");
+        document.setFontName("Arial 1");
+        document.setFontSize(12);
+        history.push(document.createState());
+
+        // Second document
+        document.setContent("Content 2");
+        document.setFontName("Arial 2");
+        document.setFontSize(14);
+        history.push(document.createState());
+
+        // Lets undo
+        document.restoreContent(history.pop());
+//        document.restoreFontName(history.pop());
+
+        System.out.println(document);
+    }
+}
